464968db07dc064eb1ffa59fd09a20a97904079d
Nimna <e1846608@bit.mrt.ac.lk>
2023-07-19 19:19:43 +0530

    displaying chart

--- src/products/utils.py
+++ src/products/utils.py
@@ -1,7 +1,8 @@
-import matplotlib.pyplot as plt
-import seaborn as sns 
+import matplotlib.pyplot as plt 
+import seaborn as sns
 from io import BytesIO
 import base64
+from django.contrib.auth.models import User
 
 def get_image():
     # create a bytes buffer for the image to save
@@ -10,38 +11,37 @@ def get_image():
     plt.savefig(buffer, format='png')
     # set the cursor the begining of the stream
     buffer.seek(0)
-    # retrieve the entire content of the file
+    # retreive the entire content of the 'file'
     image_png = buffer.getvalue()
 
-    graph = base64.b64decode(image_png)
+    graph = base64.b64encode(image_png)
     graph = graph.decode('utf-8')
 
-    # free the memory of the buffer
+    # free the memory of the buffer 
     buffer.close()
 
     return graph
 
 def get_simple_plot(chart_type, *args, **kwargs):
+    # https://matplotlib.org/faq/usage_faq.html?highlight=backend#what-is-a-backend
     plt.switch_backend('AGG')
     fig = plt.figure(figsize=(10,4))
     x = kwargs.get('x')
     y = kwargs.get('y')
     data = kwargs.get('data')
-    df = kwargs.get('df')
-
-    if chart_type=='bar plot':
-        title = "title"
+    if chart_type == 'bar plot':
+        title = "total price by day (bar)"
         plt.title(title)
-        plt.bar(x, y)
-
-    elif chart_type=='line plot':
-        title = "title"
+        plt.bar(x, y) 
+    elif chart_type == 'line plot':
+        title = "total price by day (line)"
         plt.title(title)
-        plt.bar(x, y)
+        plt.plot(x, y)
     else:
-        title = "title"
+        title = "Product count"
         plt.title(title)
         sns.countplot('name', data=data)
+    plt.xticks(rotation=45)
     plt.tight_layout()
 
     graph = get_image()
--- src/products/views.py
+++ src/products/views.py
@@ -6,29 +6,30 @@ from .utils import get_simple_plot
 # Create your views here.
 
 def chart_select_view(request):
+    graph = None
     error_message = None
     df=None
 
     product_df = pd.DataFrame(Product.objects.all().values())
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
-
+    
     if purchase_df.shape[0] > 0:
         product_df['product_id'] = product_df['id']
         df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x':'id', 'date_x':'date'}, axis=1)
+        
         if request.method == 'POST':
             chart_type = request.POST.get('sales')
             date_from = request.POST['date_from']
-            date_to = request.POST['date_to'] 
-
-            df['date'] = df['date'].apply[lambda x: x.strftime('%Y-%m-%d')]
+            date_to = request.POST['date_to']
+            print(chart_type)
+            df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
             df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
 
             if chart_type != "":
                 if date_from != "" and date_to != "":
-                    df = df[(df['date']>date_from) & (df['date']<date_to)]
+                    df = df[(df['date']>date_from) & (df['date']< date_to)]
                     df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
-                #function to get a graph
-                #get_simple_plot(chart_type, x=, y=, data=, df=)
+                graph = get_simple_plot(chart_type, x=df2['date'], y=df2['total_price'], data=df)
                     
             else:
                 error_message = "Please select a chart type to continue"
@@ -36,6 +37,7 @@ def chart_select_view(request):
         error_message = "No records found"
 
     context = {
+        'graph': graph,
         'error_message': error_message,
     }
     return render(request, 'products/main.html', context)
