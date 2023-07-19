a41b51cbfae5e564594343d4abc9d0eb656b45ac
Nimna <e1846608@bit.mrt.ac.lk>
2023-07-19 09:08:32 +0530

    defined backends

---
+++ src/products/utils.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+
+def get_simple_plot(chart_type, *args, **kwargs):
+    plt.switch_backend('AGG')
+    fig = plt.figure(figsize=(10,4))
+
+    if chart_type=='bar plot':
+        title = "title"
+        plt.title(title)
+        plt.bar(x, y)
+
+    elif chart_type=='line plot':
+        title = "title"
+        plt.title(title)
+        plt.bar(x, y)
+    else:
+        title = "title"
+        plt.title(title)
+        sns.countplot('name', data=data)
